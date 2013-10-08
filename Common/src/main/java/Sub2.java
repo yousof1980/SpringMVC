b48550789486d9e13de6888ba720d99d7f1861f4
yousof1980 <ba7472@wayne.edu>
2013-10-07 22:18:29 -0400

    good

---
+++ Common/src/main/java/Sub2.java
@@ -0,0 +1,18 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: htalsadi
+ * Date: 10/6/13
+ * Time: 7:54 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Sub2 extends Base{
+    private int y;
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
