0c69d28fcfe92ba14a78c9252e4bffcc9fb5fdb7
yousof1980 <ba7472@wayne.edu>
2013-10-07 22:28:56 -0400

    Gson POC

--- Common/src/main/java/Base.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created with IntelliJ IDEA.
- * User: htalsadi
- * Date: 10/6/13
- * Time: 7:53 PM
- * To change this template use File | Settings | File Templates.
- */
-public class Base {
-}
