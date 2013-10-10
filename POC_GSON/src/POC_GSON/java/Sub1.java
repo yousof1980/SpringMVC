67f11943659a7d58da7a45a36c2e3bc4e2423eed
yousof1980 <ba7472@wayne.edu>
2013-10-09 23:49:39 -0400

    ooo

---
+++ POC_GSON/src/POC_GSON/java/Sub1.java
@@ -0,0 +1,20 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: htalsadi
+ * Date: 10/6/13
+ * Time: 7:53 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Sub1 extends Base {
+
+    private int x;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+}
